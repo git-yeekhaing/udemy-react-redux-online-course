b9330e6d066fa3c1a1705bc5439ee85a53a82817
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-05-25 06:56:36 +0900

    learn component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React, {Component} from 'react';
 
-class App extends Component{
-  render(){
-    return(
-      <h1>Hello, World!</h1>
-      );
-  }
+
+const App = () => {
+  return(
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+    );
 }
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+
+// class App extends Component{
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">Bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}}/>
+//       </React.Fragment>
+//       );
+//   }
+// }
+
+
+// class App extends Component{
+//   render(){
+//     const greeting = "Hi, Tom";
+//     const dom = <h1>{greeting}</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     const dom = <h1>Hello, World!</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <h1>Hello, World!</h1>
+//       );
+//   }
+// }
 
 // class App extends Component{
 //   render(){
