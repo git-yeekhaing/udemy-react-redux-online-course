cb13331c337a768125d11b630b56b10099644683
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-05-25 06:56:36 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import React, {Component} from 'react';
 
 class App extends Component{
   render(){
-    return(
-      <h1>Hello, World!</h1>
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">Bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}}/>
+      </React.Fragment>
       );
   }
 }
+
+
+
+// class App extends Component{
+//   render(){
+//     const greeting = "Hi, Tom";
+//     const dom = <h1>{greeting}</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     const dom = <h1>Hello, World!</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <h1>Hello, World!</h1>
+//       );
+//   }
+// }
 
 // class App extends Component{
 //   render(){
