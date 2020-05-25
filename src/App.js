62c314d911adca8a73f397bdf672a100aa44bcd0
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-05-26 08:58:31 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,95 +1,29 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 
-const App = () => {
- const profiles = [
-  {name: "Tora", age: 10},
-  {name: "Hanako", age:5},
-  {name: "NoName", age:3}
- ]
+const App = () =>(<Counter></Counter>);
 
-  return(
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>    
-        })
-      }      
-    </div>
-    );
+class Counter extends Component {
+   constructor(props){
+    super(props)   
+    this.state = {count:0}
+   }
+
+handlePlusButton = () => {
+  this.setState({count: this.state.count + 1})
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old</div>
+handleMinusButton = () => {
+  this.setState({count: this.state.count - 1})
 }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number, isRequired
+render(){   
+    return(
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
-
-// User.defaultProps = {
-//   age:1
-// }
-
-// const App = () => {
-//   return(
-//     <div>
-//       <Cat/>
-//       <Cat/>
-//       <Cat/>
-//       <Cat/>
-//     </div>
-//     );
-// }
-
-// const Cat = () => {
-//   return <div>Meow!</div>
-// }
-
-// class App extends Component{
-//   render(){
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">Bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}}/>
-//       </React.Fragment>
-//       );
-//   }
-// }
-
-
-// class App extends Component{
-//   render(){
-//     const greeting = "Hi, Tom";
-//     const dom = <h1>{greeting}</h1>;
-//     return dom;
-//   }
-// }
-
-// class App extends Component{
-//   render(){
-//     const dom = <h1>Hello, World!</h1>;
-//     return dom;
-//   }
-// }
-
-// class App extends Component{
-//   render(){
-//     return(
-//       <h1>Hello, World!</h1>
-//       );
-//   }
-// }
-
-// class App extends Component{
-//   render(){
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello, World!!"
-//       );
-//   }
-// }
-
 export default App;
