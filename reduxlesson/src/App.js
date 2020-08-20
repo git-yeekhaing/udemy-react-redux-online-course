40e6523f62989aa09ebd6bd52025ddce428815f5
git-yeekhaing <yee.khaing.git@gmail.com>
2020-08-20 16:35:46 +0900

    props-type

--- reduxlesson/src/App.js
+++ reduxlesson/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
 
 	const profiles = [
 		{ name: "Taro", age:10 },
 		{ name: "Hanako", age:5 },
-		{ name: "NoAge" }
+		{ name: "NoAge", age:3 }
 		]
 
 	return (
@@ -25,8 +26,9 @@ const User = (props) => {
 	)
 }
 
-User.defaultProps = {
-	age:1
+User.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired
 }
 
 export default App;
