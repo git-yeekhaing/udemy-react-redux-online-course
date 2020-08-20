da2a096be88fa1b015d7cc02047463dc48cea651
git-yeekhaing <yee.khaing.git@gmail.com>
2020-08-20 11:23:09 +0900

    why-jsx

--- reduxlesson/src/App.js
+++ reduxlesson/src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div> <h1>Hello, World</h1> </div>
+    <div>Hello, World</div>
      );
 }
+
+//class App extends Component{
+//	render(){
+//		return React.createElement("div", null, "Hello, World");
+//	}
+//}
 
 export default App;
