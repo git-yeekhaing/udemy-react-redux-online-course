9b736dcdca637fbeac65a25b062986369c133387
git-yeekhaing <yee.khaing.git@gmail.com>
2020-08-21 17:39:24 +0900

    components with connect function

--- reduxlesson/src/index.js
+++ reduxlesson/src/index.js
@@ -4,7 +4,7 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
 import './index.css';
-import App from './component/App';
+import App from './components/App';
 import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
