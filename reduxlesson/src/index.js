11e4dde314b4bb784b0009eb6c520964d2683515
git-yeekhaing <yee.khaing.git@gmail.com>
2020-08-24 18:05:21 +0900

    event new page

--- reduxlesson/src/index.js
+++ reduxlesson/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from "redux-thunk";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import EventsIndex from './components/events-index';
+import EventsNew from './components/events_new';
 import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
- <Provider store={store}>
-   <EventsIndex/>
- </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>        
+        <Route exact path="/events/new" component={EventsNew}></Route>
+        <Route exact path="" component={EventsIndex}></Route>
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
